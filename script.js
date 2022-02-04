036fa49641b7c02b8bc6aff1424bff514dc02794
Makenzie Theurer <mtheurer@octolabs.io>
2022-02-03 22:52:50 -0500

    desktop popup commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showFunction() {
+    var x = document.getElementById("socialPopup");
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+}
