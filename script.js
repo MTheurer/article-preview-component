c0af0382801abe8aff1fc371f56295798dc6dfe7
Makenzie Theurer <mtheurer@octolabs.io>
2022-02-07 11:45:34 -0500

    script and style update for mobile

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function showFunction() {
-    var x = document.getElementById("socialPopup");
-    if (x.style.visibility === "hidden") {
-      x.style.visibility = "visible";
-    } else {
-      x.style.visibility = "hidden";
+  if (matchMedia('only screen and (max-width: 700px)').matches) {
+    // Mobile toggle
+    var bar = document.getElementById("account-bar");
+    if (bar.classList.contains('account-info-clicked')) {
+      bar.classList.remove("account-info-clicked");
+    }else {
+      bar.classList.add("account-info-clicked");
     }
+    return
+  }
+  
+  // Desktop toggle
+  var x = document.getElementById("socialPopup");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
